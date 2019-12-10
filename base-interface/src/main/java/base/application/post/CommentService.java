ce0a7a80429da8a79accc6719e566352738890c4
chulman <chlcjfals0122@gmail.com>
2019-12-10 22:39:24 +0900

    게시글 및 댓글 서비스 변경

--- base-interface/src/main/java/base/application/post/CommentService.java
+++ base-interface/src/main/java/base/application/post/CommentService.java
@@ -2,12 +2,15 @@ package base.application.post;
 
 import base.domain.post.CommentRepository;
 import base.domain.post.entity.Comment;
+import base.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CommentService {
     final CommentRepository commentRepository;
@@ -16,16 +19,15 @@ public class CommentService {
         return commentRepository.save(comment);
     }
 
-    public boolean deleteComment(Long commentId) {
-        Optional<Comment> comment = commentRepository.findById(commentId);
-        if (comment.isPresent()) {
-            commentRepository.delete(comment.get());
-        }
-        return comment.isPresent();
+    public Comment updateComment(Long commentId, String contents) {
+        Comment persistComment = commentRepository.getOne(commentId);
+        persistComment.update(contents);
+        return persistComment;
     }
 
-    public Comment updateComment(Long commentId, Comment comment) {
-        comment.setCommentId(commentId);
-        return commentRepository.save(comment);
+    public Comment deleteComment(Long commentId) {
+        Optional<Comment> persistComment = commentRepository.findById(commentId);
+        persistComment.ifPresent(commentRepository::delete);
+        return persistComment.orElseThrow(() -> new ResourceNotFoundException("commentId " + commentId + " not found"));
     }
 }
--- base-interface/src/main/java/base/application/post/PostService.java
+++ base-interface/src/main/java/base/application/post/PostService.java
@@ -2,15 +2,17 @@ package base.application.post;
 
 import base.domain.post.PostRepository;
 import base.domain.post.entity.Post;
+import base.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PostService {
     final PostRepository postRepository;
@@ -19,28 +21,27 @@ public class PostService {
         return postRepository.save(post);
     }
 
+    @Transactional(readOnly = true)
     public Post updatePost(Long postId, Post post) {
-        post.setPostId(postId);
-        return postRepository.save(post);
+        Post persistPost = postRepository.getOne(postId);
+        persistPost.update(post);
+        return persistPost;
     }
 
-    public boolean deletePost(Long postId) {
-        Optional<Post> post = postRepository.findById(postId);
-        if (post.isPresent()) {
-            postRepository.delete(post.get());
-        }
-        return post.isPresent();
+    public Post deletePost(Long postId) {
+        Optional<Post> persistPost = postRepository.findById(postId);
+        persistPost.ifPresent(postRepository::delete);
+        return persistPost.orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
     }
 
-    public Optional<Post> findPost(Long postId) {
-        return postRepository.findById(postId);
-    }
-
-    public Page<Post> findAllPost(Pageable pageable) {
+    @Transactional(readOnly = true)
+    public Page<Post> findAllPosts(Pageable pageable) {
         return postRepository.findAll(pageable);
     }
 
-    public List<Post> findByIdWithComments(Long postId) {
-        return postRepository.findByIdWithComments(postId);
+    @Transactional(readOnly = true)
+    public Post findByPostIdWithComments(Long postId) {
+        return Optional.of(postRepository.findByIdWithComments(postId))
+                .orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
     }
 }
