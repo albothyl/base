4d8bae88a9345568a630a93bba484210d5f7389b
grandline1 <grandline7@gmail.com>
2019-12-25 23:44:49 +0900

    header navbar 추가

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    
+    <!-- header -->
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">Home</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/boards">board</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/boards">login</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    
+    <!-- body -->
     <router-view/>
   </div>
 </template>
@@ -18,6 +38,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
