a2b987538848cc78add674af4bf92dfa856e255f
grandline1 <grandline7@gmail.com>
2019-12-27 21:50:12 +0900

    CrossOrigin 어노테이션 * 로 수정

--- base-interface/src/main/java/base/interfaces/board/controller/BoardController.java
+++ base-interface/src/main/java/base/interfaces/board/controller/BoardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*")
 public class BoardController {
 
     @Autowired
