cf65a4dfc860a52caef4ed27c25d8aff50872106
grandline1 <grandline7@gmail.com>
2019-12-28 00:32:57 +0900

    main-header 디자인 변경

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -3,75 +3,22 @@
     <!-- <img src="./assets/logo.png"> -->
     
     <!-- header -->
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="/"><i class="fas fa-home"></i>Home</b-navbar-brand>
+    <div id="header">
+      <main-header/>
+    </div>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="/boards">board</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <!-- <b-nav-item href="/boards">login</b-nav-item> -->
-          <b-nav-item href="#">token : {{token}}</b-nav-item>
-
-          <!-- login -->
-          <div>
-            <b-button id="show-btn" pill variant="primary" @click="$bvModal.show('bv-modal-login')">Login</b-button>
-            <b-modal id="bv-modal-login" hide-footer>
-              <template v-slot:modal-title>
-                Login
-              </template>
-              <div class="d-block text-center">
-                <b-form-group>
-                  <b-form-input placeholder="userid" />
-                </b-form-group>
-                <b-form-group>
-                  <b-form-input placeholder="password" />
-                </b-form-group>
-              </div>
-              <b-button block variant="primary" @click="$bvModal.hide('bv-modal-login')">login</b-button>
-              <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-login')">close</b-button>
-            </b-modal>
-          </div>
-
-          <!-- register -->
-          <div>
-            <b-button id="show-btn" pill variant="outline-danger" @click="$bvModal.show('bv-modal-register')">Register</b-button>
-            <b-modal id="bv-modal-register" hide-footer>
-              <template v-slot:modal-title>
-                Register
-              </template>
-              <div class="d-block text-center">
-                <b-form-group>
-                  <b-form-input placeholder="userid" />
-                </b-form-group>
-                <b-form-group>
-                  <b-form-input placeholder="password" />
-                </b-form-group>
-              </div>
-              <b-button block variant="danger" @click="$bvModal.hide('bv-modal-register')">register</b-button>
-              <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-register')">close</b-button>
-            </b-modal>
-          </div>
-
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    
     <!-- body -->
     <router-view/>
   </div>
 </template>
 
 <script>
+import MainHeader from './components/Header.vue'
+
 export default {
   name: 'App',
-  computed: {
-    token() { return this.$store.getters.token}
+  components: {
+    'main-header': MainHeader,
   }
 }
 </script>
