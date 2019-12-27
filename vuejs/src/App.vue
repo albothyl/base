e9d35971e9585fc333e5ed0aca891453e325ce10
grandline1 <grandline7@gmail.com>
2019-12-27 15:28:40 +0900

    header 로그인, 회원가입 디자인 추가

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -4,7 +4,7 @@
     
     <!-- header -->
     <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="/">Home</b-navbar-brand>
+      <b-navbar-brand href="/"><i class="fas fa-home"></i>Home</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -15,7 +15,50 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/boards">login</b-nav-item>
+          <!-- <b-nav-item href="/boards">login</b-nav-item> -->
+          
+          <!-- login -->
+          <div>
+            <b-button id="show-btn" pill variant="primary" @click="$bvModal.show('bv-modal-login')">Login</b-button>
+
+            <b-modal id="bv-modal-login" hide-footer>
+              <template v-slot:modal-title>
+                Login
+              </template>
+              <div class="d-block text-center">
+                <b-form-group>
+                  <b-form-input v-model="userid" placeholder="userid" />
+                </b-form-group>
+                <b-form-group>
+                  <b-form-input v-model="password" placeholder="password" />
+                </b-form-group>
+              </div>
+              <b-button block variant="primary" @click="$bvModal.hide('bv-modal-login')">login</b-button>
+              <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-login')">close</b-button>
+            </b-modal>
+          </div>
+
+          <!-- register -->
+          <div>
+            <b-button id="show-btn" pill variant="outline-danger" @click="$bvModal.show('bv-modal-register')">Register</b-button>
+
+            <b-modal id="bv-modal-register" hide-footer>
+              <template v-slot:modal-title>
+                Register
+              </template>
+              <div class="d-block text-center">
+                <b-form-group>
+                  <b-form-input v-model="userid" placeholder="userid" />
+                </b-form-group>
+                <b-form-group>
+                  <b-form-input v-model="password" placeholder="password" />
+                </b-form-group>
+              </div>
+              <b-button block variant="danger" @click="$bvModal.hide('bv-modal-register')">register</b-button>
+              <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-register')">close</b-button>
+            </b-modal>
+          </div>
+
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
