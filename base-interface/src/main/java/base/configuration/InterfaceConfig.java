44f35ca4a2620ba58bae4d5ce7dee2f284cae638
DongHwan Kim <bkuekany@gmail.com>
2020-02-12 20:46:47 +0900

    1. ModelMapper 의존성추가 및 bean 설정

--- base-interface/src/main/java/base/configuration/InterfaceConfig.java
+++ base-interface/src/main/java/base/configuration/InterfaceConfig.java
@@ -2,6 +2,8 @@ package base.configuration;
 
 import base.application.Application;
 import base.interfaces.Interfaces;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,4 +12,13 @@ import org.springframework.context.annotation.Import;
 @Import(value = { SpringSecurityConfig.class, SwaggerConfig.class })
 @ComponentScan(basePackageClasses = { Interfaces.class, Application.class })
 public class InterfaceConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return modelMapper;
+    }
 }
