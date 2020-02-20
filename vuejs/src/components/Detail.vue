ea5063c278fb1cd2add23a8963908c60e2fc41ad
grandline1 <grandline7@gmail.com>
2020-02-20 21:18:44 +0900

    게시글에 댓글생성, 댓글수정, 댓글삭제 기능 추가

--- vuejs/src/components/Detail.vue
+++ vuejs/src/components/Detail.vue
@@ -37,11 +37,21 @@
                     <b-row class="my-1">
                         <b-col sm="12">
                             <b-card class="text-center" v-for="(item, index) in comments" :key="item.commentId">
-                                <div class="bg-secondary text-light" align="left">
+                                <div v-if="item.member.memberId === 6" class="bg-secondary text-light" align="left">
+                                    <b-form-textarea v-model="item.contents"/>
+                                    <div>memberId:{{item.member.memberId}} 변경일시:{{moment(item.modifiedAt).format('YYYY-MM-DD hh:mm:ss')}}</div>
+                                    <div>
+                                        <button v-on:click="updateComment(item.commentId, item.contents)">댓글수정</button>
+                                        <button v-on:click="deleteComment(item.commentId)">댓글삭제</button>
+                                    </div>
+                                </div>
+                                <div v-else class="bg-secondary text-light"  align="left">
                                     <div>{{item.contents}}</div>
-                                    <div>{{moment(item.modifiedAt).format('YYYY-MM-DD hh:mm:ss')}}</div>
+                                    <div>memberId:{{item.member.memberId}} 변경일시:{{moment(item.modifiedAt).format('YYYY-MM-DD hh:mm:ss')}}</div>
                                 </div>
                             </b-card>
+                            <b-form-textarea v-model="comment.contents" />
+                            <button v-on:click="createComment(board.postId)">댓글생성</button>
                         </b-col>
                     </b-row>
                 </b-col>
@@ -58,6 +68,9 @@ export default {
                 postId:'',
                 title:'',
                 contents:''
+            },
+            comment:{
+                contents:'',
             },
             comments: []
         }
@@ -123,6 +136,77 @@ export default {
                 }
             }).catch(error => {
                 alert('삭제 실패! '+error)
+                console.log(error)
+            })
+        },
+        createComment(postId) {
+            this.$http.post(`/posts/comment`,{
+                contents: this.comment.contents,
+                postId: postId,
+                member: {
+                    memberId:6,
+                }
+		    },
+            {
+                headers: {
+                    Authorization : this.$store.getters.token?'Bearer '+this.$store.getters.token:null
+                }
+		    }).then((response) => {
+                if(response.data !== null){
+                    alert('댓글생성 성공!')
+                    console.log('comment create success')
+                    this.$router.go(this.$router.currentRoute)
+                } else {
+                    alert('댓글생성 실패!')
+                    console.log('comment create failed')
+                }
+		    }).catch(error => {
+                alert('댓글생성 실패! '+error)
+                console.log(error)
+            })
+        },
+        updateComment(commentId, contents) {
+            this.$http.patch(`/posts/comment/${commentId}`,{
+                contents: contents,
+                member: {
+                    memberId:6,
+                }
+		    },
+            {
+                headers: {
+                    Authorization : this.$store.getters.token?'Bearer '+this.$store.getters.token:null
+                }
+		    }).then((response) => {
+                if(response.data !== null){
+                    alert('댓글수정 성공!')
+                    console.log('comment update success')
+                    this.$router.go(this.$router.currentRoute)
+                } else {
+                    alert('댓글수정 실패!')
+                    console.log('comment update failed')
+                }
+		    }).catch(error => {
+                alert('댓글수정 실패! '+error)
+                console.log(error)
+            })
+        },
+        deleteComment(commentId) {
+            this.$http.delete(`/posts/comment/${commentId}`,{
+                headers: {
+                    Authorization : this.$store.getters.token?'Bearer '+this.$store.getters.token:null
+                }
+		    }).then((response) => {
+                console.log(response)
+                if(response.data !== null){
+                    alert('댓글삭제 성공!')
+                    console.log('comment delete success')
+                    this.$router.go(this.$router.currentRoute)
+                } else {
+                    alert('댓글삭제 실패!')
+                    console.log('comment delete failed')
+                }
+            }).catch(error => {
+                alert('댓글삭제 실패! '+error)
                 console.log(error)
             })
         }
